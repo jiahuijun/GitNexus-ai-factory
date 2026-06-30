825f3adba03672ef3eb3b02f5e610264bce6f055
Jia Huijun <553611483@qq.com>
2026-06-30 17:16:30 +0800

    fix: mock GitNexusClient in FactoryApplicationTest to restore context-load

--- src/test/java/com/factory/ai/FactoryApplicationTest.java
+++ src/test/java/com/factory/ai/FactoryApplicationTest.java
@@ -1,12 +1,16 @@
 package com.factory.ai;
 
+import com.factory.ai.gitnexus.GitNexusClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FactoryApplicationTest {
+    @MockBean
+    GitNexusClient gitNexusClient;
 
     @Test
     void contextLoads() {
